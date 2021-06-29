fc16ac3cd8362f6a1e9909b8a5c2e4c4df66db66
RBord <rostyslavbord@gmail.com>
2021-06-29 20:03:16 +0300

    Добавил линтер и хаски

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,30 +1,27 @@
-import user from '../../user.json';
-import stats from '../../statistical-data.json';
-import friends from '../../friends.json';
-import transactions from '../../transactions.json';
+import user from '../../data/user.json';
+import stats from '../../data/statistical-data.json';
+import friends from '../../data/friends.json';
+import transactions from '../../data/transactions.json';
 import { Profile } from '../Profile/Profile';
 import { Statistics } from '../Statistics/Statistics';
 import { FriendList } from '../FriendList/FriendList';
 import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
 
 export const App = () => {
-    return (
-        <div>
-            <Profile
-                name={user.name}
-                location={user.location}
-                tag={user.tag}
-                avatar={user.avatar}
-                followers={user.stats.followers}
-                views={user.stats.views}
-                likes={user.stats.likes} />
-            <Statistics
-                title = 'Upload Stats'
-                stats={stats} />
-            <FriendList
-                friends={friends} />
-            <TransactionHistory
-                items={transactions} />
-        </div>
-    );
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        location={user.location}
+        tag={user.tag}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload Stats" stats={stats} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
 };
