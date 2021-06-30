bbbaedb106a3957151b4e2fd88d7b0a002631b65
RBord <rostyslavbord@gmail.com>
2021-06-30 11:38:27 +0300

    Добавил индикаторы онлайна

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,26 +1,26 @@
 import PropTypes from 'prop-types';
-
+import s from './FriendList.module.css';
 export const FriendList = ({ friends }) => {
-    return (
-        <ul class="friend-list">
-            {friends.map(({ avatar, name, isOnline, id }) => 
-            <li class="item" key={id}>
-                <span class="status">{isOnline}</span>
-                <img class="avatar" src={avatar} alt={name} width="48" />
-                <p class="name">{name}</p>
-            </li>
-            )}
-        </ul>
-    )
-}
+  return (
+    <ul className={s.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li class="item" key={id}>
+          <span className={s[isOnline]}>{isOnline}</span>
+          <img class="avatar" src={avatar} alt={name} width="48" />
+          <p class="name">{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.shape({
-            avatar: PropTypes.string,
-            name: PropTypes.string,
-            id: PropTypes.number.isRequired,
-            isOnline: PropTypes.bool,
-        }),
-    )
-}
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+      id: PropTypes.number.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    }),
+  ),
+};
